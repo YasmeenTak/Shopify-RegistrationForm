877eb188ff6f89f9d46d56a14b975d5ad4227e94
YasmeenTak <yasmeen.tak.33@gmail.com>
2020-10-30 01:25:18 +0200

    fixing

--- js/loginViewModule.js
+++ js/loginViewModule.js
@@ -17,33 +17,33 @@ var LoginViewModel = function () {
       return;
     }
   };
+
+  self.hasBeenSubmitted = ko.observable(false);
+
+  window.firstName = self.firstName;
+
+  self.handleSubmit = () => {
+    //Check for errors
+    var errors = ko.validation.group(self);
+    if (errors().length > 0) {
+      errors.showAllMessages();
+      return;
+    }
+
+    self.hasBeenSubmitted(true);
+
+    //Form is valid
+    console.log('submit the form!');
+    //Api call would go here
+    //
+    //
+    console.log({
+      emailAddress: self.emailAddress(),
+      userPassword: self.userPassword(),
+      //subscriptionType: self.subscriptionType(),
+    });
+  };
 };
-//   self.hasBeenSubmitted = ko.observable(false);
 
-//   window.firstName = self.firstName;
-
-//   self.handleSubmit = () => {
-//     //Check for errors
-//     var errors = ko.validation.group(self);
-//     if (errors().length > 0) {
-//       errors.showAllMessages();
-//       return;
-//     }
-
-//     self.hasBeenSubmitted(true);
-
-//     //Form is valid
-//     console.log('submit the form!');
-//     //Api call would go here
-//     //
-//     //
-//     console.log({
-//       emailAddress: self.emailAddress(),
-//       userPassword: self.userPassword(),
-//       //subscriptionType: self.subscriptionType(),
-//     });
-//   };
-// };
-
-// const knockoutApp = document.querySelector('#knockout-app');
-// ko.applyBindings(new LoginViewModel(), knockoutApp);
+const knockoutApp = document.querySelector('#knockout-app');
+ko.applyBindings(new LoginViewModel(), knockoutApp);
